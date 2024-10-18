87bd7cbc8059c5fba5afc5276cfdef6304fbf3f4
Abhay Singh <abhay982997@gmail.com>
2024-10-18 11:30:11 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+body{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+   position: relative;
+   top: 200px;
+}
+
+.container{
+    border: 3px solid rgb(212, 209, 26);
+    width: 470px;
+    height: 200px;
+    text-align: center;
+    background-color: red;
+    border-radius: 30px;
+}
+.container button{
+    height: 50px;
+    position: relative;
+   top: 23px;
+   background-color: rgb(43, 13, 216);
+   color: aquamarine;
+   border-radius: 20px;
+}
+
